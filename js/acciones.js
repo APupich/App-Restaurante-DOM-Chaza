2387fe2e991b54ba88645ce50ec2963449992914
APupich <agustinpupich173@gmail.com>
2024-05-21 20:33:06 -0300

    comiteo

--- js/acciones.js
+++ js/acciones.js
@@ -1,1 +1,29 @@
-console.log("Hello world");
+let cantidad = document.getElementById("cantidad");
+let btnRestar = document.getElementById("restar");
+let btnSumar = document.getElementById("sumar");
+let cant = 1;
+
+cantidad.innerText = cant;
+btnRestar.innerText = "block";
+function restar() {
+    if (cant > 1) {
+        btnRestar.innerText = 'remove';
+        btnSumar.innerText = 'add';
+        cant = cant - 1;
+        cantidad.innerText = cant;
+    }else{
+        btnRestar.innerText = 'block';
+    }
+}
+
+function sumar() {
+    if (cant < 15) {
+        btnRestar.innerText = 'remove';
+        btnSumar.innerText = 'add';
+        cant = cant + 1;
+    cantidad.innerText = cant;
+    }else{
+        btnSumar.innerText = 'block';
+    }
+    
+}
