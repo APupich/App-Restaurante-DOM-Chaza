56ee1febacd41cb922dae7f3bc7ab9d6efd9273b
APupich <agustinpupich173@gmail.com>
2024-06-04 20:18:42 -0300

    coeraiubgfpesfe

--- js/leerjson.js
+++ js/leerjson.js
@@ -1,38 +1,15 @@
 let body = document.getElementById("resto");
-function estrellas(estrellas){
-    const estrella = /*html*/`<span class="iconos verde f13">star</span>`
-    const estrellaG = /*html*/`<span class="iconos f13">star</span>`
-    let acum = "";
-    for (let i = 0; i < estrellas; i++) {
-        acum += estrella;
-    }
-    for (let i = 0; i < 5-estrellas; i++) {
-        acum += estrellaG;
-    }
-    return acum;
-}
-function precio(dolares){
-    const dol = /*html*/`<span class="iconos verde f13">euro_symbol</span>`
-    const dolG = /*html*/`<span class="iconos f13">euro_symbol</span>`
-    let acum = "";
-    for (let i = 0; i < dolares; i++) {
-        acum += dol;
-    }
-    for (let i = 0; i < 3-dolares; i++) {
-        acum += dolG;
-        
-    }
-    return acum;
-}
+
+
 
 fetch('js/restos.json')
-.then(response => {
-    return response.json()
+    .then(response => {
+        return response.json()
     })
-.then(data => {
-    data.forEach(e => {
-        body.innerHTML += /*html*/
-        `<a  href="restaurante.html" id="${e.name}">
+    .then(data => {
+        data.forEach(e => {
+            body.innerHTML += /*html*/
+                `<a id="${e.name}" class="restaurante">
         <article class="oferta">
             <div class="img">
                 <img src="${e.avatar.src}">
@@ -44,21 +21,30 @@ fetch('js/restos.json')
                 <span>${e.open} - ${e.close}</span>
                 <div class="stats">
                     <div class="estrellas">
-                       ${estrellas(e.stars)}<span>(${e.cant_review})</span>
+                       ${`<span class="iconos verde f13">star</span>`.repeat(e.stars)}
+                       ${`<span class="iconos f13">star</span>`.repeat(5-e.stars)}
+                       <span>(${e.cant_review})</span>
                     </div>
                     <div class="valor">
-                    ${precio(e.price)}
+                        ${`<span class="iconos verde f13">euro_symbol</span>`.repeat(e.price)}
+                        ${`<span class="iconos f13">euro_symbol</span>`.repeat(3-e.price)}
                     </div>
-
                 </div>
             </div>
             <div class="calificacion">${e.rating}</div>
         </article>
     </a>`
-    });
-})
 
-addEventListener(click, (e) =>{
-    let click = document.getElementById()
-    console.log(e.currentTarget);
-})
+
+        });
+    })
+
+// addEventListener(click, (e) =>{
+//     let click = document.getElementById()
+//     let restoId = document.querySelector(".calificacion")
+//     console.log(e.currentTarget);
+// })
+
+// function obtenerId(){
+//     let restoId = document.querySelector(".calificacion")
+// }
